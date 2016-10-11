34c5ab911d9700e6e17f36c9dcd9df35aee88d50
Danlan Chen <danlanchenchen@gmail.com>
2016-10-11 14:14:09 +0900

    Create load_numpy.py

---
+++ as3_digit_recognition/load_numpy.py
@@ -0,0 +1,15 @@
+import numpy as np
+def loadNumpy(file):
+	x = np.load(file)
+	return x
+def getNumpy():
+	file1 = 'train_inputs.npy'
+	file2 = 'train_outputs.npy'
+	file3 = 'test_inputs.npy'
+	train_x = loadNumpy(file1)
+	train_y = loadNumpy(file2)
+	test_x = loadNumpy(file3)
+	print train_x.shape
+	print train_y.shape
+	print test_x.shape
+	return train_x,train_y,test_x
